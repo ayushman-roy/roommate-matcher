18c1816e98992c1fb2112c1bd0d431a269dd2890
Ayushman Roy <ayushmanroy09@gmail.com>
2024-08-11 14:14:33 +0530

    final commit based on data structures

--- roommate_match.py
+++ roommate_match.py
@@ -1,21 +1,27 @@
 import pandas as pd
 import numpy as np
 from scipy.spatial import distance  
+import csv
+
 
 # code preferences using numbers and attach prefernce weights
 # clean data as per format on the test_data_RA.csv
 # append another collumn for roommate messages within the csv
 # change location for accessing said data below
-students = pd.read_csv('/Users/ayushmanroy/Desktop/Roommate_Matching/test_data_RA.csv')
+students = pd.read_csv('/Users/ayushmanroy/Code/Roommate_Matching/DATA.csv')
 
 # checking if data imported
 print(students.head())
 
-# select columns with preferences
-selected_columns = [2,4,6]
+# encode the textual preferences as numeric values
+# ensure that encoding takes into account how preferences are aligned and synergic 
+# allot weights for each preference and then take the weighted preferences as final values
 
-# student as a vector list for matching
-studentList = [(row[0], row[7], tuple(row[i] for i in selected_columns)) for row in students.values]
+# select columns with weighted preferences
+selected_columns = [1,2,3,4,5,6,7]
+
+# student as a vector list for matching where student identifier must the first column
+studentList = [(row[0], tuple(row[i] for i in selected_columns)) for row in students.values]
 
 # check the cleaned list
 print(studentList)
@@ -26,7 +32,7 @@ def find_closest_neighbors(data):
     
     while len(data) > 1:
         # extract vectors from the data
-        vectors = np.array([item[2] for item in data])
+        vectors = np.array([item[1] for item in data])
         
         # calculate distances between all pairs
         distances = distance.cdist(vectors, vectors, 'euclidean')
@@ -38,7 +44,7 @@ def find_closest_neighbors(data):
         min_index = np.unravel_index(np.argmin(distances), distances.shape)
         
         # corresponding names and messages and append to the result
-        closest_pairs.append((((data[min_index[0]][0], data[min_index[0]][1]), data[min_index[1]][0], data[min_index[1]][1])))
+        closest_pairs.append((((data[min_index[0]][0]), data[min_index[1]][0])))
         # remove the matched elements 
         data = [data[i] for i in range(len(data)) if i not in min_index]
     
@@ -46,5 +52,17 @@ def find_closest_neighbors(data):
 
 closest_neighbors = find_closest_neighbors(studentList)
 
-# export as excel maybe
 print(closest_neighbors)
+
+# exporting the allocated roommates as .csv
+
+filename = "/Users/ayushmanroy/Code/Roommate_Matching/Allocation.csv"
+
+with open(filename, 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Roommate 1', 'Roommate 2'])
+    
+    # Writing the data
+    writer.writerows(closest_neighbors)
+
+print(f"Data has been written to {filename}")
